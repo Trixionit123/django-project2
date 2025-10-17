bae0d5258e85c4fe819a857a935a02a0a1c8e088
Trixionit <trixionit@example.com>
2025-10-17 14:29:41 +0300

    Revert PR #7 (merge 0dbaf79)

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from decimal import Decimal
 from .models import Order, OrderItem
 
 
@@ -20,10 +19,10 @@ class OrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         items_data = validated_data.pop('items', [])
         order = Order.objects.create(**validated_data)
-        total = Decimal('0')
+        total = 0
         for item in items_data:
             order_item = OrderItem.objects.create(order=order, **item)
-            total += (Decimal(order_item.price) * order_item.quantity)
+            total += order_item.price * order_item.quantity
         order.total_price = total
         order.save()
         return order
--- orders/web_urls.py
+++ orders/web_urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .web_views import OrderListView, OrderCreateView
+from . import web_views
 
 
 urlpatterns = [
-    path('orders/', OrderListView.as_view(), name='order_list'),
-    path('orders/create/', OrderCreateView.as_view(), name='order_create'),
+    path('orders/', web_views.order_list, name='order_list'),
+    path('orders/create/', web_views.order_create, name='order_create'),
 ]
 
 
